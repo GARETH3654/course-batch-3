0cc4421d617a997fb55cf425e7c83932483446d8
ivan lezama nuñez <gareth.ivc@gmail.comwqqwwqgit config --global user.email gareth.ivc@gmail.com>
2018-05-25 22:08:12 -0500

    primera pagina

---
+++ frontend_avanzado/Practica 1/index.js
@@ -0,0 +1,24 @@
+'use strict'
+console.log('hola, soy ivan');
+
+
+var modal = document.getElementById('myModal');
+
+var btn = document.getElementById("myBtn");
+
+
+var span = document.getElementsByClassName("close")[0];
+
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
